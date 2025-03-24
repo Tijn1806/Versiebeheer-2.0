ff1c7a41b4ad26ba6bcd973f5ab07596ac974ceb
Tijn1806 <Tijn@kromjong.nl>
2025-03-24 13:47:21 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,1 +1,19 @@
+<?php
+$servername = "localhost";  // Database host
+$username = "root";        // Your MySQL username
+$password = "";            // Your MySQL password
+$dbname = "team4";         // Your database name
 
+$dsn = "mysql:host=$servername;dbname=$dbname;charset=utf8mb4";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?> 
